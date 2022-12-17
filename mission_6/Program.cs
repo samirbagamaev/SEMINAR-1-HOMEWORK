e41a90bfcfe4760d3276c15779745ea225cb1b74
Samir Bagamaev <05ru4315@gmail.com>
2022-12-17 23:43:37 +0300

    Задача 4

---
+++ mission_6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число и выдает,является ли число четным
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ if (number % 2==1)
+            {
+                Console.WriteLine("число нечетное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
+ 
+            Console.ReadKey();
+
+ 
